8ec032316d5eadd0f646cf281bc31312a4ce9311
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-05-08 16:04:55 -0500

    added get analytics and task queue

---
+++ server/Types.ts
@@ -0,0 +1,26 @@
+
+
+interface Contract{
+    id: string,                     // id and address are the same
+    address: string,
+    name: string,
+}
+
+interface NFT{
+    id: string,                     // id and transactionId are the same
+    transactionId: string,
+    contractAddress: string,
+    mintTime: Date,
+    tokenId: number,
+    owner: string, 
+    tokenURI: string,
+}
+
+interface TaskQueueItem{
+    id: string,
+    type: string,
+    import: Date,
+    data: Object
+}
+
+export type {Contract, NFT, TaskQueueItem};
--- server/api.ts
+++ server/api.ts
@@ -2,18 +2,71 @@ import express from 'express';
 import { getFirestore } from 'firebase-admin/firestore';
 import {initializeApp, cert} from 'firebase-admin/app';
 import { cwd } from 'process';
-
+import { TaskQueueItem } from './Types';
 
 const api = express();
 
-// const serviceAccount = require(cwd() + '/firebase_credentials.json');
-// initializeApp({
-//   credential: cert(serviceAccount),
-// });
-// const database = getFirestore();
+const serviceAccount = require(cwd() + '/firebase_credentials.json');
+initializeApp({
+  credential: cert(serviceAccount),
+});
+const database = getFirestore();
 
 
 const APP_PORT = 3000;
+
+api.get("/api/analytics/get" , async (req, res) => {
+    try{
+        const analyticsDocument = await database.collection("analytics").doc("analytics").get();
+        if (analyticsDocument.exists){
+            const analytics = analyticsDocument.data();
+            res.status(200).json({
+                success: true,
+                ...analytics
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                error: "Failed to retrieve Analytics"
+            });
+        }
+    } catch{
+        res.status(400).json({
+            success: false,
+            error: "Something went wrong -- please try again!"
+        });
+    }
+
+});
+
+api.get("/api/taskQueue/get", async (req, res) => {
+    try{
+        const taskQueueNfts = await database.collection("task_queue").where("type", "==", "NFT").get();
+        const taskQueueContracts = await database.collection("task_queue").where("type", "==", "contract").get();
+        
+        const nfts: TaskQueueItem[] = [];
+        const contracts: TaskQueueItem[] = [];
+        
+        taskQueueContracts.forEach((contract) => {
+            contracts.push(contract.data() as TaskQueueItem);
+        })
+
+        taskQueueNfts.forEach((nft) => {
+            nfts.push(nft.data() as TaskQueueItem);
+        })
+
+        res.status(200).json({
+            success: true,
+            contracts: contracts,
+            nfts: nfts
+        });
+    } catch {
+        res.status(400).json({
+            success: false,
+            error: "Something went wrong -- please try again!"
+        });
+    }
+})
 
 api.get("/", (req, res) => {
     res.status(200).json({
