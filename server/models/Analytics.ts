4156cea850f393ab90335be9cb1b6c2760417577
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-07-27 22:29:20 -0500

    updated analytics layer

--- server/models/Analytics.ts
+++ server/models/Analytics.ts
@@ -4,24 +4,59 @@ const AnalyticsSchema = new Schema(
     {
       totalContracts: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
       },
       totalERC1155: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
       },
       totalERC721: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
       },
       totalEthereumNFTs: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
       },
       totalNFTs: {
         type: Number,
-        required: true
+        required: true,
+        default: 0
       },
+      contractsSuccess: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      contractsFailure: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      nftSuccess: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      nftFailure: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      searchApiSuccess: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      searchApiFailure: {
+        type: Number,
+        required: true,
+        default: 0
+      }
     }
 );
 
