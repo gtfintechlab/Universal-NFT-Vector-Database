912d8d83e4c62474f8089d7e6a9bf0e19ff9761f
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-08-04 18:32:52 -0500

    added aws region on add sqs

--- client/server/utils/SQS.ts
+++ client/server/utils/SQS.ts
@@ -4,6 +4,10 @@ export async function addTaskIdSQS (taskId) {
   const secrets = useRuntimeConfig().secretVariables
   process.env.AWS_ACCESS_KEY_ID = secrets.AWS_ACCESS_KEY_ID
   process.env.AWS_SECRET_ACCESS_KEY = secrets.AWS_SECRET_ACCESS_KEY
+  AWS.config.update({
+    region: "us-east-1",
+  })
+
   const sqs = new AWS.SQS()
   const params = {
     QueueUrl: secrets.TASK_QUEUE_SQS_URL as string,
